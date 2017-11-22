b65a19b42a8d3fa9866e04d6b09b323684c662c3
Sajid Rabbani <sajid@nascenia.com>
2017-11-22 09:22:32 +0600

    Take input of address
    - require yargs for input handling
    - request to google map api based on the input address
    - handle error for invalid address

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var address = encodeURIComponent(argv.address);
+console.log(address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=5/1%20Lalmatia%20Dhaka',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  if (error){
+    console.log('Unable to connect to Google server');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to fine the address');
+  } else if (body.status === 'OK') {
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
